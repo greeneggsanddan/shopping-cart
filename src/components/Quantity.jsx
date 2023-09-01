63db537166b8102c16643f7c7273f63f0f9c7d70
greeneggsanddan <greeneggsanddan@gmail.com>
2023-09-01 00:00:23 -0700

    Manage the state of the Quantity component

--- src/components/Quantity.jsx
+++ src/components/Quantity.jsx
@@ -1,11 +1,37 @@
-import './Quantity.css'
+import { useState } from 'react';
+import './Quantity.css';
 
 export default function Quantity() {
+  const [quantity, setQuantity] = useState(1);
+
+  function subtractQuantity() {
+    if (quantity > 1) setQuantity(quantity - 1);
+  }
+
+  function addQuantity() {
+    if (quantity < 99) setQuantity(quantity + 1);
+  }
+
+  function handleInput(e) {
+    setQuantity(parseInt(e.target.value, 10));
+  }
+
   return (
-    <div className='input-container'>
-      <button type="button">-</button>
-      <input className='quantity' type="number" value="1" />
-      <button type="button">+</button>
+    <div className="input-container">
+      <button type="button" onClick={subtractQuantity}>
+        -
+      </button>
+      <input
+        className="quantity"
+        type="number"
+        min="1"
+        max="99"
+        value={quantity}
+        onChange={handleInput}
+      />
+      <button type="button" onClick={addQuantity}>
+        +
+      </button>
       <button type="submit">ADD TO CART</button>
     </div>
   );
