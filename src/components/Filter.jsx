243634cd4da35c0fa5f5381b416d29b7edb77b34
greeneggsanddan <greeneggsanddan@gmail.com>
2023-09-05 22:25:15 -0700

    Add filter assets

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,26 @@
 import './Filter.css';
+import white from '../assets/W.svg'
+import blue from '../assets/U.svg'
+import black from '../assets/B.svg'
+import red from '../assets/R.svg'
+import green from '../assets/G.svg'
+import colorless from '../assets/C.svg'
+import zero from '../assets/0.svg'
+import one from '../assets/1.svg'
+import two from '../assets/2.svg'
+import three from '../assets/3.svg'
+import four from '../assets/4.svg'
+import five from '../assets/5.svg'
+import six from '../assets/6.svg'
+import seven from '../assets/7.svg'
+import creature from '../assets/creature.svg'
+import planeswalker from '../assets/planeswalker.svg'
+import instant from '../assets/instant.svg'
+import sorcery from '../assets/sorcery.svg'
+import enchantment from '../assets/enchantment.svg'
+import artifact from '../assets/artifact.svg'
+import land from '../assets/land.svg'
+
 
 export default function Filter({
   color,
@@ -45,41 +67,96 @@ export default function Filter({
 
   return (
     <div className="filter">
-      <label htmlFor="search-bar" hidden>Search Bar</label>
+      <label htmlFor="search-bar" hidden>
+        Search Bar
+      </label>
       <input id="search-bar" value={query} onChange={handleChange} />
-      <div className="color">
-        <button onClick={() => filterColor('W')}>White</button>
-        <button onClick={() => filterColor('U')}>Blue</button>
-        <button onClick={() => filterColor('B')}>Black</button>
-        <button onClick={() => filterColor('R')}>Red</button>
-        <button onClick={() => filterColor('G')}>Green</button>
-        <button onClick={() => filterColor('C')}>Colorless</button>
+      <div className="colors">
+        <button onClick={() => filterColor('W')}>
+          <img className="mana-symbol" src={white} alt="white mana symbol" />
+        </button>
+        <button onClick={() => filterColor('U')}>
+          <img className="mana-symbol" src={blue} alt="blue mana symbol" />
+        </button>
+        <button onClick={() => filterColor('B')}>
+          <img className="mana-symbol" src={black} alt="black mana symbol" />
+        </button>
+        <button onClick={() => filterColor('R')}>
+          <img className="mana-symbol" src={red} alt="red mana symbol" />
+        </button>
+        <button onClick={() => filterColor('G')}>
+          <img className="mana-symbol" src={green} alt="green mana symbol" />
+        </button>
+        <button onClick={() => filterColor('C')}>
+          <img
+            className="mana-symbol"
+            src={colorless}
+            alt="colorless mana symbol"
+          />
+        </button>
       </div>
-      <div className="mana-value">
-        <button onClick={() => filterManaValue(0)}>0</button>
-        <button onClick={() => filterManaValue(1)}>1</button>
-        <button onClick={() => filterManaValue(2)}>2</button>
-        <button onClick={() => filterManaValue(3)}>3</button>
-        <button onClick={() => filterManaValue(4)}>4</button>
-        <button onClick={() => filterManaValue(5)}>5</button>
-        <button onClick={() => filterManaValue(6)}>6</button>
-        <button onClick={() => filterManaValue(7)}>7+</button>
+      <div className="mana-values">
+        <button onClick={() => filterManaValue(0)}>
+          <img className="mana-symbol" src={zero} alt="zero mana cost" />
+        </button>
+        <button onClick={() => filterManaValue(1)}>
+          <img className="mana-symbol" src={one} alt="one mana cost" />
+        </button>
+        <button onClick={() => filterManaValue(2)}>
+          <img className="mana-symbol" src={two} alt="two mana cost" />
+        </button>
+        <button onClick={() => filterManaValue(3)}>
+          <img className="mana-symbol" src={three} alt="three mana cost" />
+        </button>
+        <button onClick={() => filterManaValue(4)}>
+          <img className="mana-symbol" src={four} alt="four mana cost" />
+        </button>
+        <button onClick={() => filterManaValue(5)}>
+          <img className="mana-symbol" src={five} alt="five mana cost" />
+        </button>
+        <button onClick={() => filterManaValue(6)}>
+          <img className="mana-symbol" src={six} alt="six mana cost" />
+        </button>
+        <button onClick={() => filterManaValue(7)}>
+          <img className="mana-symbol" src={seven} alt="seven mana cost" />
+        </button>
       </div>
-      <div className="type">
-        <button onClick={() => filterType('Creature')}>Creature</button>
-        <button onClick={() => filterType('Planeswalker')}>Planeswalker</button>
-        <button onClick={() => filterType('Instant')}>Instant</button>
-        <button onClick={() => filterType('Sorcery')}>Sorcery</button>
-        <button onClick={() => filterType('Enchantment')}>Enchantment</button>
-        <button onClick={() => filterType('Artifact')}>Artifact</button>
-        <button onClick={() => filterType('Battle')}>Battle</button>
-        <button onClick={() => filterType('Land')}>Land</button>
+      <div className="types">
+        <button onClick={() => filterType('Planeswalker')}>
+          <img className="mana-symbol" src={planeswalker} alt="planeswalker symbol" />
+        </button>
+        <button onClick={() => filterType('Creature')}>
+          <img className="mana-symbol" src={creature} alt="creature symbol" />
+        </button>
+        <button onClick={() => filterType('Instant')}>
+          <img className="mana-symbol" src={instant} alt="instant symbol" />
+        </button>
+        <button onClick={() => filterType('Sorcery')}>
+          <img className="mana-symbol" src={sorcery} alt="sorcery symbol" />
+        </button>
+        <button onClick={() => filterType('Enchantment')}>
+          <img className="mana-symbol" src={enchantment} alt="enchantment symbol" />
+        </button>
+        <button onClick={() => filterType('Artifact')}>
+          <img className="mana-symbol" src={artifact} alt="artifact symbol" />
+        </button>
+        <button onClick={() => filterType('Land')}>
+          <img className="mana-symbol" src={land} alt="land symbol" />
+        </button>
       </div>
-      <div className="rarity">
-        <button onClick={() => filterRarity('mythic')}>Mythic</button>
-        <button onClick={() => filterRarity('rare')}>Rare</button>
-        <button onClick={() => filterRarity('uncommon')}>Uncommon</button>
-        <button onClick={() => filterRarity('common')}>Common</button>
+      <div className="rarities">
+        <button onClick={() => filterRarity('mythic')}>
+          <div className='mythic rarity'></div>
+        </button>
+        <button onClick={() => filterRarity('rare')}>
+          <div className='rare rarity'></div>
+        </button>
+        <button onClick={() => filterRarity('uncommon')}>
+          <div className="uncommon rarity"></div>
+        </button>
+        <button onClick={() => filterRarity('common')}>
+          <div className="common rarity"></div>
+        </button>
       </div>
       <button onClick={resetFilter}>Reset</button>
     </div>
