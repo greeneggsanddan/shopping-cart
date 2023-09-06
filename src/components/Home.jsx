b91d526cafdc9ff42cc53828efd47aed580580b0
greeneggsanddan <greeneggsanddan@gmail.com>
2023-09-05 17:56:34 -0700

    Style the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom";
+import './Home.css';
 
 export default function Home() {
   return (
-    <div>
-      Wilds of Eldraine out now! Shop here: <Link to={'shop/woe'}>Wilds of Eldraine</Link>
+    <div className="home">
+      <div className="hero"></div>
+      <div className="overlay">
+        Wilds of Eldraine out now!
+        <Link to={'shop/woe'}>
+          <button className="shop-here" type="button">
+            SHOP HERE
+          </button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
